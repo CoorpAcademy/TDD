8335e3a05164914b33c6e64446e511dbbc765344
BertrandD <bernaz@pm.me>
2018-12-21 15:32:05 +0100

    start testing postin

--- tests/day_15.test.ts
+++ tests/day_15.test.ts
@@ -1,4 +1,4 @@
-import test from "ava";
+import test from 'ava';
 import {
   stringToPlan,
   planToString,
@@ -10,18 +10,18 @@ import {
   isFinish,
   getResult,
   getOrderedUnitPositions
-} from "../day_15";
+} from '../day_15';
 
-test("stringToPlan", t => {
+test('stringToPlan', t => {
   const actual = stringToPlan(`#######
 #.G.E.#
 #E.G.E#
 #.G.E.#
 #######`);
-  const WALL: Wall = { type: "wall" };
-  const CAVERN: Cavern = { type: "cavern" };
-  const GOBLIN: Goblin = { type: 'goblin', hitPoint: 200};
-  const ELF: Elf = { type: 'elf', hitPoint: 200};
+  const WALL: Wall = { type: 'wall' };
+  const CAVERN: Cavern = { type: 'cavern' };
+  const GOBLIN: Goblin = { type: 'goblin', hitPoint: 200 };
+  const ELF: Elf = { type: 'elf', hitPoint: 200 };
   const expected: Plan = [
     [WALL, WALL, WALL, WALL, WALL, WALL, WALL],
     [WALL, CAVERN, GOBLIN, CAVERN, ELF, CAVERN, WALL],
@@ -32,13 +32,11 @@ test("stringToPlan", t => {
   t.deepEqual(actual, expected);
 });
 
-
-
 test('planToString', t => {
-  const WALL: Wall = { type: "wall" };
-  const CAVERN: Cavern = { type: "cavern" };
-  const GOBLIN: Goblin = { type: 'goblin', hitPoint: 200};
-  const ELF: Elf = { type: 'elf', hitPoint: 200};
+  const WALL: Wall = { type: 'wall' };
+  const CAVERN: Cavern = { type: 'cavern' };
+  const GOBLIN: Goblin = { type: 'goblin', hitPoint: 200 };
+  const ELF: Elf = { type: 'elf', hitPoint: 200 };
   const plan: Plan = [
     [WALL, WALL, WALL, WALL, WALL, WALL, WALL],
     [WALL, CAVERN, GOBLIN, CAVERN, ELF, CAVERN, WALL],
@@ -51,57 +49,71 @@ test('planToString', t => {
 #.G.E.#
 #E.G.E#
 #.G.E.#
-#######`
+#######`;
   t.deepEqual(actual, expected);
 });
 
 test('isFinish', t => {
-  t.is(isFinish(stringToPlan(`#######
+  t.is(
+    isFinish(
+      stringToPlan(`#######
 #E..EG#
 #.#G.E#
 #E.##E#
 #G..#.#
 #..E#.#
-#######`)), false)
-  t.is(isFinish(stringToPlan(`#######
+#######`)
+    ),
+    false
+  );
+  t.is(
+    isFinish(
+      stringToPlan(`#######
 #.E.E.#
 #.#E..#
 #E.##.#
 #.E.#.#
 #...#.#
-#######`)), true)
+#######`)
+    ),
+    true
+  );
 });
 
 test('getResult', t => {
-  const WALL: Wall = { type: "wall" };
-  const CAVERN: Cavern = { type: "cavern" };
-  const ELF = (hitPoint: number): Elf => ({ type: 'elf', hitPoint});
-  t.is(getResult([
-    [WALL, WALL, WALL, WALL, WALL, WALL, WALL],
-    [WALL, CAVERN, ELF(164), CAVERN, ELF(197), WALL, WALL],
-    [WALL, CAVERN, WALL, ELF(200), CAVERN, CAVERN, WALL],
-    [WALL, ELF(98), CAVERN, WALL, WALL, CAVERN, WALL],
-    [WALL, CAVERN, ELF(200), CAVERN, WALL, CAVERN, WALL],
-    [WALL, CAVERN, CAVERN, CAVERN, WALL, CAVERN, WALL],
-    [WALL, WALL, WALL, WALL, WALL, WALL, WALL]
-  ], 46), 39514)
+  const WALL: Wall = { type: 'wall' };
+  const CAVERN: Cavern = { type: 'cavern' };
+  const ELF = (hitPoint: number): Elf => ({ type: 'elf', hitPoint });
+  t.is(
+    getResult(
+      [
+        [WALL, WALL, WALL, WALL, WALL, WALL, WALL],
+        [WALL, CAVERN, ELF(164), CAVERN, ELF(197), WALL, WALL],
+        [WALL, CAVERN, WALL, ELF(200), CAVERN, CAVERN, WALL],
+        [WALL, ELF(98), CAVERN, WALL, WALL, CAVERN, WALL],
+        [WALL, CAVERN, ELF(200), CAVERN, WALL, CAVERN, WALL],
+        [WALL, CAVERN, CAVERN, CAVERN, WALL, CAVERN, WALL],
+        [WALL, WALL, WALL, WALL, WALL, WALL, WALL]
+      ],
+      46
+    ),
+    39514
+  );
 });
 
 test('getOrderedUnitPositions', t => {
   t.deepEqual(
-    getOrderedUnitPositions(stringToPlan(`#######
+    getOrderedUnitPositions(
+      stringToPlan(`#######
 #.G.E.#
 #E.G.E#
 #.G.E.#
-#######`)),
-    [
-      [1, 2],
-      [1, 4],
-      [2, 1],
-      [2, 3],
-      [2, 5],
-      [3, 2],
-      [3, 4]
-    ]
-  )
+#######`)
+    ),
+    [[1, 2], [1, 4], [2, 1], [2, 3], [2, 5], [3, 2], [3, 4]]
+  );
 });
+
+const plan = [[]];
+
+test('findClosestEnemy -   ', t => t.pass());
