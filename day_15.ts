66278efe5e6e13861da9c7380066baad9ccf9e83
godu <arthur.weber@viacesi.fr>
2018-12-21 16:13:05 +0100

    Adds string utils

--- day_15.ts
+++ day_15.ts
@@ -1,28 +1,23 @@
 const ATTACK_POINT = 3;
 const INITIAL_HIT_POINT = 200;
 
-type Wall = {type: 'wall'};
-type Goblin = {
+export type Wall = {type: 'wall'};
+export type Goblin = {
     type: 'goblin',
-    hitPoint: number,
-    position: Coordinate
+    hitPoint: number
 };
-type Elf = {
+export type Elf = {
     type: 'elf',
-    hitPoint: number,
-    position: Coordinate
+    hitPoint: number
 };
-type Cavern = {type: 'cavern'};
-type Cell = Wall | Goblin | Elf | Cavern;
-type Plan = Array<Array<Cell>>;
-
-const charToCell = (char: string): Cell =>
-    ({type: 'wall'});
+export type Cavern = {type: 'cavern'};
+export type Cell = Wall | Goblin | Elf | Cavern;
+export type Plan = Array<Array<Cell>>;
 
 type Coordinate = [number, number];
 type Unit = Elf|Goblin;
 type Round = {
-    units : Array<Unit>
+    plan : Plan
 }
 
 const findClosestEnemy = (plan: Plan, unit: Unit, enimies: Array<Unit>): Coordinate => 
@@ -32,4 +27,44 @@ const getAdjacentCell = (position: Coordinate): Array<Coordinate> => [];
 
 const getFirstPosition = (positions: Array<Coordinate>): Coordinate => [0, 0];
 
-const planToString = (plan: Plan): string => '';
+const charToCell = (char: string): Cell => {
+    switch (char) {
+        case '#': {
+            const wall: Wall = {
+                type: 'wall',
+            };
+            return wall;
+        }
+        case 'G': {
+            const goblin: Goblin = {
+                type: 'goblin',
+                hitPoint: INITIAL_HIT_POINT,
+            };
+            return goblin;
+        }
+        case 'E': {
+            const elf: Elf = {
+                type: 'elf',
+                hitPoint: INITIAL_HIT_POINT,
+            };
+            return elf;
+        }
+        default: {
+            const cavern: Cavern = {type : 'cavern'};
+            return cavern;
+        }
+    }
+}
+const cellToChar = (cell: Cell): string => {
+    switch(cell.type) {
+        case 'wall': return '#';
+        case 'goblin': return 'G';
+        case 'elf': return 'E';
+        default: return '.'
+    }
+};
+
+export const planToString = (plan: Plan): string =>
+    plan.map(row => row.map(cellToChar).join('')).join('\n');
+export const stringToPlan = (input: string): Plan => 
+    input.split('\n').map(row => row.split('').map(charToCell));
--- tests/day_15.test.ts
+++ tests/day_15.test.ts
@@ -1,4 +1,53 @@
 import test from "ava";
-// import {} from "../day_15";
- 
-test('test', t => t.pass());
+import {
+  stringToPlan,
+  planToString,
+  Plan,
+  Wall,
+  Cavern,
+  Goblin,
+  Elf
+} from "../day_15";
+
+test("stringToPlan", t => {
+  const actual = stringToPlan(`#######
+#.G.E.#
+#E.G.E#
+#.G.E.#
+#######`);
+  const WALL: Wall = { type: "wall" };
+  const CAVERN: Cavern = { type: "cavern" };
+  const GOBLIN: Goblin = { type: 'goblin', hitPoint: 200};
+  const ELF: Elf = { type: 'elf', hitPoint: 200};
+  const expected: Plan = [
+    [WALL, WALL, WALL, WALL, WALL, WALL, WALL],
+    [WALL, CAVERN, GOBLIN, CAVERN, ELF, CAVERN, WALL],
+    [WALL, ELF, CAVERN, GOBLIN, CAVERN, ELF, WALL],
+    [WALL, CAVERN, GOBLIN, CAVERN, ELF, CAVERN, WALL],
+    [WALL, WALL, WALL, WALL, WALL, WALL, WALL]
+  ];
+  t.deepEqual(actual, expected);
+});
+
+
+
+test('planToString', t => {
+  const WALL: Wall = { type: "wall" };
+  const CAVERN: Cavern = { type: "cavern" };
+  const GOBLIN: Goblin = { type: 'goblin', hitPoint: 200};
+  const ELF: Elf = { type: 'elf', hitPoint: 200};
+  const plan: Plan = [
+    [WALL, WALL, WALL, WALL, WALL, WALL, WALL],
+    [WALL, CAVERN, GOBLIN, CAVERN, ELF, CAVERN, WALL],
+    [WALL, ELF, CAVERN, GOBLIN, CAVERN, ELF, WALL],
+    [WALL, CAVERN, GOBLIN, CAVERN, ELF, CAVERN, WALL],
+    [WALL, WALL, WALL, WALL, WALL, WALL, WALL]
+  ];
+  const actual = planToString(plan);
+  const expected = `#######
+#.G.E.#
+#E.G.E#
+#.G.E.#
+#######`
+  t.deepEqual(actual, expected);
+});
